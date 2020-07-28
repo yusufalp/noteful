72850db39d183a2c2e7823d1a5a06e9f55d38aa7
Beyonce Knowles <beyonce@thinkful.com>
2020-07-28 13:30:26 -0700

    Added notes into folders, and clickable folders, notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link, Switch } from 'react-router-dom';
+import Sidebar from './Components/Sidebar';
+import FolderPage from './Components/FolderPage';
+import NotePage from './Components/NotePage';
+import STORE from './Components/STORE';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    folders: STORE.folders,
+    notes: STORE.notes
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className='header'>
+          <Link to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}>Noteful</Link>
+        </header>
+        <aside>
+          <Switch>
+            <Route path='/notes/:noteId'
+              render={routerProps => (
+                <Sidebar
+                  notes={this.state.notes}
+                  folders={this.state.folders}
+                  history={routerProps.history}
+                  match={routerProps.match} />
+              )}
+            />
+            <Route path='/'
+              render={routerProps => (
+                <Sidebar
+                  notes={this.state.notes}
+                  folders={this.state.folders}
+                  history={routerProps.history}
+                  match={routerProps.match} />
+              )}
+            />
+          </Switch>
+        </aside>
+        <main>
+          <Route exact path='/'
+            render={routerProps => (
+              <FolderPage notes={this.state.notes} match={routerProps.match} />
+            )}
+          />
+          <Route
+            path='/folders/:folderId'
+            render={routerProps => (
+              <FolderPage notes={this.state.notes} match={routerProps.match} />
+            )}
+          />
+          <Route
+            path='/notes/:noteId'
+            render={routerProps => (
+              <NotePage notes={this.state.notes} match={routerProps.match} />
+            )}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
